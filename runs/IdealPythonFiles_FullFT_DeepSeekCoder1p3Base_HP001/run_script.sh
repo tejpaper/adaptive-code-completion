31b4517b37f9c777aeddb55896339385652f9188
tejpaper <dev.assistant.re@gmail.com>
2024-11-13 19:35:03 +0000

    New runs

---
+++ runs/IdealPythonFiles_FullFT_DeepSeekCoder1p3Base_HP001/run_script.sh
@@ -0,0 +1,6 @@
+/home/sapronov/.virtualenvs/lca-solvers/bin/python3 \
+/home/sapronov/lca-solvers/pipeline/__main__.py \
+run_name=IdealPythonFiles_FullFT_DeepSeekCoder1p3Base_HP001 \
+composer=chained_composer/ideal_python_files \
++additional_composer=chained_composer/filled_python_files \
++additional_preprocessor=completion_loss_preprocessor/full_completion_loss_16k
